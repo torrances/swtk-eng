48424e19b445add25f7f85aa2e9f45161af90452
Craig Trim <cmtrim@us.ibm.com>
2016-01-06 17:28:01 -0800

    add test case

---
+++ eng-preprocessor/src/test/java/org/swtk/eng/preprocess/CreateSentences012Test.java
@@ -0,0 +1,27 @@
+package org.swtk.eng.preprocess;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Test;
+import org.swtk.eng.preprocess.functions.CreateSentences;
+
+public class CreateSentences012Test {
+
+	@Test
+	public void run12() throws Throwable {
+
+		List<String> list = new ArrayList<String>();
+		list.add("This is my sentence. That was it. Here goes nothing!");
+
+		Collection<String> lines = CreateSentences.process(list);
+		assertNotNull(lines);
+		for (String line : lines)
+			System.err.println(line);
+		assertEquals(3, lines.size());
+	}
+}
